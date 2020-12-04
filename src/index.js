6d87abe655a7501f82d73e0816e2c03539e867aa
Vyacheslav <v.a.buglov@gmail.com>
2020-12-04 20:28:13 +0300

    v1.0.4

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greeter = void 0;
+var greeter = function (name) { return "Hello " + name; };
+exports.greeter = greeter;
